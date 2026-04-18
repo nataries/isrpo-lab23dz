a1d0737014c5e53cf1c328f861915650d364a700
nataries <natalizastavnaya@icloud.com>
2026-04-18 13:27:57 +0300

    Implemented form validation

---
+++ profile/main.js
@@ -0,0 +1,14 @@
+function saveData() {
+    const name = document.getElementById("name").value.trim();
+const age = document.getElementById("age").value.trim();
+const city = document.getElementById("city").value.trim();
+const hobby = document.getElementById("hobby").value.trim();
+    if (!name || !age || !city || !hobby) {
+        alert("Заполните все поля");
+        return;
+    }
+
+    const output = `Имя: ${name} <br> Возраст: ${age} <br> Город: ${city} <br> Хобби: ${hobby}`;
+
+    document.getElementById("output").innerHTML = output;
+}
